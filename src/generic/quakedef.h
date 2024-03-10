014728deecb43bf56ffbb80030f4d865bca0df81
Tyler Young <tyleryoung88@yahoo.com>
2024-03-10 11:46:33 -0400

    [CLIENT] Clean up Quake files

--- src/generic/quakedef.h
+++ src/generic/quakedef.h
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 //define	PARANOID			// speed sapping error checking
 
-#define	GAMENAME	"nzp"
+#define	GAMENAME	"id1"
 
 #include <math.h>
 #include <string.h>
